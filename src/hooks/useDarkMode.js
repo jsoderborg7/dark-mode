52f944bb2abecf27134928192e19bdf648f4bc6a
Jenn Soderborg <jsoderborg7@gmail.com>
2019-09-04 14:49:49 -0600

    step 2 complete, useDarkMode set up

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+import useLocalStorage from './useLocalStorage';
+
+export const useDarkMode = () =>{
+  const [darkMode, setDarkMode] = useLocalStorage('dark')
+
+  useEffect(() =>{
+    if (darkMode === true)
+      document.querySelector('body').classList.add('dark-mode')
+    else
+      document.querySelector('body').classList.remove('dark-mode')
+  }, [darkMode])
+
+  return [darkMode, setDarkMode]
+}
